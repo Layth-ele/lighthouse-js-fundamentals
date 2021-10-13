19296b532cb160b966ce6267b29fb0e631b4da27
‘layth-ele’ <layth.ele@gmail.com>
2021-10-13 23:22:23 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,10 @@
+/*
+############################################################################
+
+   if Statements
+############################################################################
+*/
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////exercise 1//////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 
@@ -51,3 +58,62 @@
 // }
 
 // console.log("Now you're ready to go outside!");
+
+// const temperature = -1;  /// the program to tell us to pick a scarf
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+// const temperature = 16 ;  /// the program to tell us Short sleeves are fine.
+
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+/*
+############################################################################
+
+   Logical operators
+############################################################################
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////exercise 1//////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////exercise 2//////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// let temperature = 43;
+
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////exercise 3//////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// let raining = false;
+
+// if (!raining) {
+//   console.log("Leave your umbrella at home!");
+// }
