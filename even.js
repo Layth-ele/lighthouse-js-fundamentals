3e32a09fe86bf24184f6f7091977ef45b5c7ec4b
‘layth-ele’ <layth.ele@gmail.com>
2021-10-14 19:11:57 +0000

    difference between calling console.log within a function and within return value

---
+++ even.js
@@ -0,0 +1,36 @@
+/*
+############################################################################
+
+                          <<<<< isEven function >>>>
+############################################################################
+*/
+
+/*
+//////////////////////////////////////////////////////////////////////////////
+                                 exercise 1
+                set our function return values to new variables
+//////////////////////////////////////////////////////////////////////////////
+*/
+
+// const isEven = function (num) {
+//   return num % 2 === 0;
+// };
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+/*
+//////////////////////////////////////////////////////////////////////////////
+                                 exercise 2
+             without setting our function return values to new variables
+//////////////////////////////////////////////////////////////////////////////
+*/
+
+// const isEven = function (num) {
+//   return num % 2 === 0;
+// };
+
+// console.log(isEven(10));
+// console.log(isEven(11));
