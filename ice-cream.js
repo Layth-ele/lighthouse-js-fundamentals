9bf46eeb18a48f4bab76254d1d60dc125dce1588
‘layth-ele’ <layth.ele@gmail.com>
2021-10-15 01:59:33 +0000

    add ice-cream js file array practice purposes

---
+++ ice-cream.js
@@ -0,0 +1,23 @@
+/*
+############################################################################
+
+                          <<<<< Arrays - Ice cream! >>>>
+############################################################################
+*/
+
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours[5] = "root beer";
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);
